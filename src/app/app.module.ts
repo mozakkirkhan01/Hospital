658be9dd4eea618dae9850a1071759fba9dc0599
Md Mozakkir Khan <mozakkirkhan337@gmail.com>
2025-11-26 18:44:21 +0530

    service-charge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerMod
 import { FormComponent } from './admin/form/form.component';
 import { ManageDoctorComponent } from './admin/manage-doctor/manage-doctor.component';
 import { ManagePatientComponent } from './admin/manage-patient/manage-patient.component';
+import { ServiceChargeComponent } from './admin/service-charge/service-charge.component';
 
 
 @NgModule({
@@ -70,6 +71,7 @@ import { ManagePatientComponent } from './admin/manage-patient/manage-patient.co
     FormComponent,
     ManageDoctorComponent,
     ManagePatientComponent,
+    ServiceChargeComponent,
   ],
   imports: [
     BrowserModule,
