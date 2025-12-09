96bf1895bbd5bc094d3d1aff59c49c85663bdf03
Md Mozakkir Khan <mozakkirkhan337@gmail.com>
2025-12-09 14:08:55 +0530

    opd

---
+++ src/app/admin/manage-opd/manage-opd.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-opd',
+  templateUrl: './manage-opd.component.html',
+  styleUrls: ['./manage-opd.component.css']
+})
+export class ManageOpdComponent {
+
+}
